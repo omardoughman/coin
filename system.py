ad01e7e940b04253d2939879771524605b07ba75
omardoughman <238817456+omardoughman@users.noreply.github.com>
2026-05-07 15:24:03 +0100

    Delete system.py

--- system.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def check_system_status():
-    if os.path.exists("main.codec"):
-        print("System Codec detected. Processing binary data...")
-
-if __name__ == "__main__":
-    check_system_status()

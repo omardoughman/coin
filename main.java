2fbcb4db2be0128329e9316633f748d902dab749
omardoughman <238817456+omardoughman@users.noreply.github.com>
2026-05-07 15:51:40 +0100

    Load native library and update main method output

--- main.java
+++ main.java
@@ -1,5 +1,15 @@
 public class main {
+    // This is the "Magic Link" that connects Java to your C/C++ files
+    static {
+        System.loadLibrary("coin_engine");
+    }
+
+    // This declares that the actual code for this function is in a C file
+    public native void runEngine();
+    public native void checkSecurity(String pass);
+
     public static void main(String[] args) {
-        System.out.println("Java Entry Point Active.");
+        System.out.println("Java Layer: Loading Engine...");
+        new main().runEngine();
     }
 }
